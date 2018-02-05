1e0f8ec2945e566f3f8b1f3f1962707ebcccefcc
jill1231 <1003750594@qq.com>
2018-02-05 18:41:00 +0800

    源码解读 － dropRight函数

--- drop.js
+++ drop.js
@@ -32,10 +32,8 @@ function drop(array, n=1) {
   //通过三目运算符判断是否传入一个数组，若是，则将长度设置为数组的长度，否则，设置为0
   const length = array == null ? 0 : array.length
   //通过三目运算符判断长度是否大于0 ，若是，再通过三目运算符判断传入的裁剪个数是否小于0，若是，设置为0，否则设置为传入的值，再调用slice函数进行裁剪，否则返回空数组
-  return length
-    ? slice(array, n < 0 ? 0 : n, length)
-    : []
+  return length ? slice(array, n < 0 ? 0 : n, length) : []
 }
 
-//
+//想想：从右边开始裁剪数组中的 N 个数组，返回剩余的部分
 export default drop
--- dropRight.js
+++ dropRight.js
@@ -22,8 +22,16 @@ import slice from './slice.js'
  * dropRight([1, 2, 3], 0)
  * // => [1, 2, 3]
  */
+/**
+ * 从右边开始裁剪数组中的 N 个数组，返回剩余的部分
+ * @param array 需要处理的数组
+ * @param n 裁剪的个数,设置默认参数为1
+ * @returns {Array} 返回数组剩余的部分
+ */
 function dropRight(array, n=1) {
+  //通过三目运算符判断是否传入一个数组，若是，则将长度设置为数组的长度，否则，设置为0
   const length = array == null ? 0 : array.length
+  //slice函数的第二个参数，若传入的数值是负数，则与数组长度相加
   return length ? slice(array, 0, n < 0 ? 0 : -n) : []
 }
 
