eb131fb143c40ed14cc7300a9ae6f2bc37dd3f54
jill1231 <1003750594@qq.com>
2018-02-05 18:18:22 +0800

    源码解读 － drop函数

--- drop.js
+++ drop.js
@@ -22,11 +22,20 @@ import slice from './slice.js'
  * drop([1, 2, 3], 0)
  * // => [1, 2, 3]
  */
+/**
+ * 裁剪数组中的前 N 个数组，返回剩余的部分。
+ * @param array 需要处理的数组
+ * @param n 裁剪的个数,设置默认参数为1
+ * @returns {Array} 返回数组的剩余的部分。
+ */
 function drop(array, n=1) {
+  //通过三目运算符判断是否传入一个数组，若是，则将长度设置为数组的长度，否则，设置为0
   const length = array == null ? 0 : array.length
+  //通过三目运算符判断长度是否大于0 ，若是，再通过三目运算符判断传入的裁剪个数是否小于0，若是，设置为0，否则设置为传入的值，再调用slice函数进行裁剪，否则返回空数组
   return length
     ? slice(array, n < 0 ? 0 : n, length)
     : []
 }
 
+//
 export default drop
