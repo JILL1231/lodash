bcd62d17f29a8faf915ec208c4b2f53dd33de136
jill1231 <1003750594@qq.com>
2018-02-08 18:25:36 +0800

    源码解读 － isNull函数

--- isNull.js
+++ isNull.js
@@ -13,6 +13,12 @@
  * isNull(void 0)
  * // => false
  */
+/**
+ * 检查参数是否为null，若是返回true，否则返回false
+ * @param value
+ * @returns {boolean}
+ */
+//null 与 undefined 是它们所属类型的唯一一个值。所以判断方法非常方便，只要我们使用恒等比较===
 function isNull(value) {
   return value === null
 }
